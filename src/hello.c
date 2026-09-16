d81fbbdc8ccb9dfd1a8c4a8471c3b24f202a32d3
Lasse-RTU <lasse-mads.fenske@edu.rtu.lv>
2026-09-16 15:29:13 +0300

    Initial commit

--- src/hello.c
+++ src/hello.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  
+  printf("Hello, RTU World from C Lab in 2026!\n");
+
+  char name[50];
+
+  printf("Please enter your first name: ");
+  scanf("%49s", name);
+  printf("%d", i);
+  printf("\nHello %s! Nice to meet you!\n", name);
+
   printf("You passed %d argument(s).\n", argc - 1);
+
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
